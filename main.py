c6cf6209dc30393dcc3f603afcb4f9b278b021b5
JameeWise <jamee.wise@yahoo.com>
2021-02-17 19:47:58 -0500

    initial

--- main.py
+++ main.py
@@ -1,53 +1,75 @@
-# -*- coding: utf-8 -*-
-
 import random
-
 import simpy
 
+class Car:
 
-RANDOM_SEED = 1431221
-NEW_CUSTOMERS = 20  # Total number of customers
-INTERVAL_CUSTOMERS = 10.0  # Generate new customers roughly every x seconds
+    carNumber = 0
+
+    def __init__(self, env, res):
+        self.env = env
+        self.res = res
+        Car.carNumber += 1
+        self.name = Car.carNumber
+
+    def drive(self):
+        self.arrivalTime = self.env.now
+        print('Arrival::', self)
+        req =  self.res[0].request()
+        yield req
+        print('Start ordering::', self)
+        self.orderTime = random.expovariate(1.0/12.0)
+        evt = self.env.timeout(self.orderTime)
+        yield evt
+        print("Finish ordering::", self)
+
+        self.foodPrepTime = 3.0 #random.expovariate(1.0/5.0)
+        foodPrep = self.env.timeout(self.foodPrepTime)
+
+        self.res[0].release(req)
+
+        print('Waiting for pickup::', self)
+
+        req =  self.res[2].request()
+        yield req
+
+        print('Start pickup::', self)
+        self.pickupTime = 2.0 #random.expovariate(1.0/3.0)
+        pickup = self.env.timeout(self.pickupTime)
+
+        val = yield pickup & foodPrep  # simpy.anyOf(evt,evt,evt)
+
+        print("Finish pickup::", self)
+
+        if pickup in val:
+            print('food was ready waited to pay')
+        elif foodPrep in val:
+            print("food wasn't ready had to wait")
+        else:
+            print("oops")
+
+        self.res[2].release(req)
 
 
-def source(env, number, interval, counter):
-    """Source generates customers randomly"""
-    for i in range(number):
-        c = customer(env, f'Customer{i:2d}', counter, meanServiceTime=15.0)
-        env.process(c)
-        t = random.expovariate(1.0 / interval)
-        yield env.timeout(t)
+    def __str__(self):
+        return f'Car: {self.name:d} time: {self.env.now:.3f}'
 
+def arrivalGen(env, res):
 
-def customer(env, name, teller, meanServiceTime):
-    """Customer arrives, is served and leaves."""
-    arrive = env.now
-    print(f'Time: {arrive:0.3f} Customer: {name:s}')
+    while True:
 
-    req = teller.request()
+        c = Car(env, res)
+        env.process(c.drive())
+        evt = env.timeout(random.expovariate(1.0/10.0))
+        yield evt
 
-    yield req
-
-    wait = env.now - arrive
-
-    print(f'Time: {env.now:0.3f} Customer: {name:s} Waited: {wait:0.3f}')
-
-    serviceTime = random.expovariate(1.0 / meanServiceTime)
-
-    yield env.timeout(serviceTime)
-
-    print(f'Time: {env.now:0.3f} Customer: {name:s} leaves')
-
-    teller.release(req)
-
-
-# Setup and start the simulation
-print('Simply single server queue')
-random.seed(RANDOM_SEED)
 
 env = simpy.Environment()
-# Start processes and run
-teller = simpy.Resource(env, capacity=1)
-env.process(source(env, NEW_CUSTOMERS, INTERVAL_CUSTOMERS, teller))
+orderWindow = simpy.Resource(env, 1)
+payWindow = simpy.Resource(env,1)
+pickupWindow = simpy.Resource(env,1)
 
-env.run()
+res = [orderWindow, payWindow, pickupWindow]
+
+env.process(arrivalGen(env, res))
+
+env.run(until = 100.0)
