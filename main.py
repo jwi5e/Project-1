5d73e8d15a3d9857327a5131839f5d5e3896c8a8
JameeWise <jamee.wise@yahoo.com>
2021-02-18 20:32:02 -0500

    REQUESTS

--- main.py
+++ main.py
@@ -17,34 +17,43 @@ class Car:
         print('Arrival::', self)
         req = self.res[0].request()
         yield req
+        self.res[0].release(req)
         print('Start ordering::', self)
+        order = self.res[1].request()
+        yield order
         self.orderTime = random.weibullvariate(3, 1.5)
         evt = self.env.timeout(self.orderTime)
         yield evt
         print("Finish ordering::", self)
+        self.res[1].release(order)
 
         self.foodPrepTime = random.weibullvariate(6, 2.0)
         foodPrep = self.env.timeout(self.foodPrepTime)
-        self.res[0].release(req)
-        req = self.res[1].request()
+        req = self.res[2].request()
         yield req
         print('Start paying::', self)
+        self.res[2].release(req)
+        req = self.res[3].request()
+        yield req
         self.payTime = random.weibullvariate(2, 1.5)
         pay = self.env.timeout(self.payTime)
         yield pay
         print("Finish paying::", self)
-        self.res[1].release(req)
+        self.res[3].release(req)
 
         print('Waiting for pickup::', self)
 
-        req = self.res[2].request()
+        req = self.res[4].request()
         yield req
 
         print('Start pickup::', self)
+        self.res[4].release(req)
+        req = self.res[5].request()
+        yield req
         self.pickupTime = random.weibullvariate(2, 1.5)
         pickup = self.env.timeout(self.pickupTime)
 
-        val = yield pickup & foodPrep & pay # simpy.anyOf(evt,evt,evt)
+        val = yield pickup & foodPrep # simpy.anyOf(evt,evt,evt)
 
         print("Finish pickup::", self)
 
@@ -52,12 +61,10 @@ class Car:
             print('food was ready waited to pay')
         elif foodPrep in val:
             print("food wasn't ready had to wait")
-        elif pay in val:
-            print("idk")
         else:
             print("oops")
 
-        self.res[2].release(req)
+        self.res[5].release(req)
 
     def __str__(self):
         return f'Car: {self.name:d} time: {self.env.now:.3f}'
@@ -82,7 +89,7 @@ betweenPayAndPickup = simpy.Resource(env, 1)
 pickupWindow = simpy.Resource(env,1)
 
 
-res = [orderWindow, payWindow, pickupWindow]
+res = [initialLine, orderWindow, betweenOrderAndPay, payWindow, betweenPayAndPickup, pickupWindow]
 
 env.process(arrivalGen(env, res))
 
