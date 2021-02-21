f58b5c4dfcd104145860e9ba57b71618c7533702
Nick Gross <nick.gross15@gmail.com>
2021-02-21 14:18:18 -0500

    big yoshi

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import random
 import simpy
 
+
+SERVED = 0
+LEFT = 0
+SERVICETIME = 0
+SERVED_DATA = []
+BALK_DATA = []
+TIME_DATA = []
+
 class Car:
 
     carNumber = 0
@@ -14,41 +22,76 @@ class Car:
     def drive(self):
         self.arrivalTime = self.env.now
         print('Arrival::', self)
-        req =  self.res[0].request()
+        req = self.res[0].request()
         yield req
+        if (res[0].count == 7):
+            roll = random.randint(0, 100)
+            if (roll >= 80):
+                self.res[0].release(req)
+                print("homie dipped::", self)
+                global LEFT
+                LEFT += 1
+                return
+
+
+        order = self.res[1].request()
+        yield order
+        self.res[0].release(req)
         print('Start ordering::', self)
-        self.orderTime = random.expovariate(1.0/12.0)
+        self.orderTime = random.weibullvariate(3, 1.5)
         evt = self.env.timeout(self.orderTime)
         yield evt
         print("Finish ordering::", self)
-
-        self.foodPrepTime = 3.0 #random.expovariate(1.0/5.0)
+        self.foodPrepTime = random.weibullvariate(6, 2.0)
         foodPrep = self.env.timeout(self.foodPrepTime)
 
-        self.res[0].release(req)
 
+        req2 = self.res[2].request()
+        yield req2
+        self.res[1].release(order)
+
+
+        req3 = self.res[3].request()
+        yield req3
+        self.res[2].release(req2)
+        print('Start paying::', self)
+        self.payTime = random.weibullvariate(2, 1.5)
+        pay = self.env.timeout(self.payTime)
+        yield pay
+        print("Finish paying::", self)
+
+
+        req4 = self.res[4].request()
+        yield req4
+        self.res[3].release(req3)
         print('Waiting for pickup::', self)
 
-        req =  self.res[2].request()
-        yield req
 
+        req5 = self.res[5].request()
+        yield req5
+        self.res[4].release(req4)
         print('Start pickup::', self)
-        self.pickupTime = 2.0 #random.expovariate(1.0/3.0)
+        self.pickupTime = random.weibullvariate(2, 1.5)
         pickup = self.env.timeout(self.pickupTime)
-
-        val = yield pickup & foodPrep  # simpy.anyOf(evt,evt,evt)
+        val = yield pickup & foodPrep # simpy.anyOf(evt,evt,evt)
 
         print("Finish pickup::", self)
-
+        
         if pickup in val:
             print('food was ready waited to pay')
         elif foodPrep in val:
             print("food wasn't ready had to wait")
         else:
             print("oops")
-
-        self.res[2].release(req)
-
+        
+        global SERVED
+        SERVED += 1
+        
+        endTime = self.env.now
+        global SERVICETIME
+        SERVICETIME += endTime - self.arrivalTime
+        
+        self.res[5].release(req5)
 
     def __str__(self):
         return f'Car: {self.name:d} time: {self.env.now:.3f}'
@@ -59,17 +102,25 @@ def arrivalGen(env, res):
 
         c = Car(env, res)
         env.process(c.drive())
-        evt = env.timeout(random.expovariate(1.0/10.0))
+        evt = env.timeout(random.expovariate(1.0/5.0))
         yield evt
 
 
 env = simpy.Environment()
+initialLine = simpy.Resource(env, 7)
 orderWindow = simpy.Resource(env, 1)
+betweenOrderAndPay = simpy.Resource(env, 4)
 payWindow = simpy.Resource(env,1)
+betweenPayAndPickup = simpy.Resource(env, 1)
 pickupWindow = simpy.Resource(env,1)
 
-res = [orderWindow, payWindow, pickupWindow]
+
+res = [initialLine, orderWindow, betweenOrderAndPay, payWindow, betweenPayAndPickup, pickupWindow]
 
 env.process(arrivalGen(env, res))
 
-env.run(until = 100.0)
+env.run(until=120.0)
+print('SERVED = ', SERVED)
+print('LEFT = ', LEFT)
+print('SERVICETIME = ', SERVICETIME/SERVED)
+
